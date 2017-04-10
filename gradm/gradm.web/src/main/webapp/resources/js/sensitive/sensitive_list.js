9ca8ff70bfa490781473b8d5dbdd1eceb03ab54b
yeyuZoro <lfl31645@163.com>
2017-04-10 21:22:59 +0800

    sensitive_list.js fix

--- gradm/gradm.web/src/main/webapp/resources/js/sensitive/sensitive_list.js
+++ gradm/gradm.web/src/main/webapp/resources/js/sensitive/sensitive_list.js
@@ -1,9 +1,9 @@
 $(document).ready(function($){
     var pageNum = 1;
-    #("searchForm").submit(function(){
+    $("searchForm").submit(function(){
         pageNum = 1;
         moreData($(this), pageNum, false);
         return false;
-    })
+    });
     $("#searchForm").submit();
 });
